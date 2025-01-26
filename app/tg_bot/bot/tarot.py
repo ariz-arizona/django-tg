dd9084b79a80f63aedb898a637af1a80ec5619c4
aarizona <ar.ariz.arizona@gmail.com>
2025-01-26 23:56:16 +0300

    more card

--- app/tg_bot/bot/tarot.py
+++ app/tg_bot/bot/tarot.py
@@ -1,8 +1,14 @@
 import re
 import random
 from typing import List, Optional
-from telegram import Update, InputMediaPhoto
-from telegram.ext import CommandHandler, MessageHandler, CallbackContext, filters
+from telegram import Update, InputMediaPhoto, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    CallbackContext,
+    filters,
+)
 
 from django.utils.timezone import now
 from django.core.exceptions import ObjectDoesNotExist
@@ -26,7 +32,9 @@ class TarotBot(AbstractBot):
                 & filters.ChatType.PRIVATE
                 & filters.Regex(r"^\/card(\d+)?"),
                 self.handle_card,
-            )
+            ),
+            CallbackQueryHandler(self.handle_more_button, pattern=r"^more_\d+_.+$"),
+            CallbackQueryHandler(self.handle_desc_button, pattern=r"^desc$"),
         ]
 
     async def get_deck(self, index=None):
@@ -57,8 +65,12 @@ class TarotBot(AbstractBot):
         counter: int = 1,
         card_ids: Optional[List[str]] = None,  # Используем card_id (str)
         major: bool = False,
+        exclude_cards: Optional[List[str]] = None,  # Новый параметр для исключения карт
     ) -> List[dict]:
         try:
+            if card_ids is None:
+                card_ids = []
+
             # 1. Проверка существования колоды
             if not await TarotDeck.objects.filter(id=deck_id).aexists():
                 raise ValueError(f"Колода {deck_id} не найдена")
@@ -98,6 +110,12 @@ class TarotBot(AbstractBot):
                 )
             ]
 
+            if exclude_cards:
+                exclude_cards = [
+                    str(cid) for cid in exclude_cards
+                ]  # Преобразуем в строки
+                all_card_ids = [cid for cid in all_card_ids if cid not in exclude_cards]
+
             # 5. Вычисляем оставшиеся карты
             remaining = max(0, counter - len(manual_cards))
             exclude_ids = {card.tarot_card.card_id for card in manual_cards}
@@ -120,6 +138,7 @@ class TarotBot(AbstractBot):
             combined = manual_cards + random_cards
             return [
                 {
+                    "card_id": card.tarot_card.card_id,
                     "img_id": card.img_id,
                     "name": card.tarot_card.name,
                     "flipped": random.choice([True, False]),
@@ -131,6 +150,32 @@ class TarotBot(AbstractBot):
             raise ValueError("Карта не найдена") from e
         except Exception as e:
             raise RuntimeError(f"Ошибка: {str(e)}") from e
+
+    async def send_card(self, update: Update, cards, exclude_cards, deck, major):
+        await update.effective_message.reply_media_group(
+            # [InputMediaPhoto(c["img_id"], c["name"]) for c in cards],
+            [
+                InputMediaPhoto("https://placecats.com/300/200", c["name"])
+                for c in cards
+            ],
+            reply_to_message_id=update.effective_message.message_id,
+        )
+        await update.effective_message.reply_text(
+            "\n".join([deck.name, deck.link]),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Еще карту",
+                            callback_data=f'more_{deck.id}_{"#".join(exclude_cards)}_{int(major)}',
+                        ),
+                        InlineKeyboardButton("Базовые значения", callback_data="desc"),
+                    ]
+                ]
+            ),
+            reply_to_message_id=update.effective_message.message_id,
+            disable_web_page_preview=True,
+        )
 
     async def handle_card(self, update: Update, context: CallbackContext):
         """
@@ -174,24 +219,48 @@ class TarotBot(AbstractBot):
         # Парсинг флага major
         options["major"] = bool(re.search(r"major", msg_text))
 
-        logger.info(f"Options: {options}")
-        await update.message.reply_text(
-            options, reply_to_message_id=update.message.message_id
-        )
-
         deck = await self.get_deck(options["deck"])
-        await update.message.reply_text(
-            deck.name, reply_to_message_id=update.message.message_id
-        )
-
         cards = await self.get_cards(
             deck.id, options["counter"], options["card_ids"], options["major"]
         )
-        await update.message.reply_text(
-            cards, reply_to_message_id=update.message.message_id
+        await self.send_card(
+            update, cards, [c["card_id"] for c in cards], deck, options["major"]
         )
-        await update.message.reply_media_group(
-            # [InputMediaPhoto(c["img_id"], c["name"]) for c in cards],
-            [InputMediaPhoto("https://placecats.com/300/200", c["name"]) for c in cards],
-            reply_to_message_id=update.message.message_id
+
+    async def handle_more_button(self, update: Update, context: CallbackContext):
+        query = update.callback_query
+        await query.answer()
+
+        _, deck_id, exclude_cards, major = query.data.split("_")
+        exclude_cards = exclude_cards.split("#") if exclude_cards else []
+
+        logger.info(
+            f"Запрошена ещё одна карта для колоды {deck_id}. "
+            f"Исключаемые карты: {exclude_cards}"
         )
+
+        deck = await self.get_deck(int(deck_id))
+        new_card = await self.get_cards(
+            deck_id=int(deck_id),
+            counter=1,
+            card_ids=None,
+            exclude_cards=exclude_cards,
+            major=bool(major),
+        )
+
+        if not new_card:
+            await query.edit_message_text("Нет доступных карт для выбора.")
+            return
+        full_exclude = exclude_cards + [c["card_id"] for c in new_card]
+        await self.send_card(
+            update,
+            new_card,
+            full_exclude,
+            deck,
+            bool(major),
+        )
+
+    async def handle_desc_button(self, update: Update, context: CallbackContext):
+        query = update.callback_query
+        await query.answer()
+        logger.info(query)

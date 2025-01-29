c51bcfa77825ec032e76a30c6e5c1762a3c4324e
aarizona <ar.ariz.arizona@gmail.com>
2025-01-29 23:09:16 +0300

    extended meanings paged

--- app/server/settings.py
+++ app/server/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = [
 TG_WEBHOOK_HOST = f"https://{os.environ.get('TG_WEBHOOK_HOST')}"
 PICTURE_CHAT = os.environ.get("PICTURE_CHAT")
 PARSER_URL = os.environ.get("PARSER")
+TG_DEBUG = os.environ.get("TG_DEBUG").lower() == "true"
+
 CSRF_TRUSTED_ORIGINS = (TG_WEBHOOK_HOST,)
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
--- app/tg_bot/bot/tarot.py
+++ app/tg_bot/bot/tarot.py
@@ -1,7 +1,7 @@
 import re
 import random
 import time
-from typing import List, Optional
+from typing import List, Optional, Dict
 from telegram import Update, InputMediaPhoto, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     CommandHandler,
@@ -15,7 +15,7 @@ from django.utils.timezone import now
 from django.core.exceptions import ObjectDoesNotExist
 
 from tg_bot.bot.abstract import AbstractBot
-from tg_bot.models import TgUser, TarotDeck, TarotCardItem, TarotCard
+from tg_bot.models import TgUser, TarotDeck, TarotCardItem, TarotCard, ExtendedMeaning
 
 from server.logger import logger
 from django.conf import settings
@@ -35,31 +35,34 @@ class TarotBot(AbstractBot):
                 self.handle_card,
             ),
             CallbackQueryHandler(self.handle_more_button, pattern=r"^more_\d+_.+$"),
-            CallbackQueryHandler(self.handle_desc_button, pattern=r"^desc_(\d+(?:#\d+)*)$"),
-            CallbackQueryHandler(self.handle_pagination, pattern=r"^meaning_\d+_\d+$"),
+            CallbackQueryHandler(
+                self.handle_desc_button, pattern=r"^desc_(\d+(?:#\d+)*)$"
+            ),
+            CallbackQueryHandler(
+                self.handle_pagination, pattern=r"^meaning_[a-z]+_\d+_\d+$"
+            ),
         ]
 
-    async def get_deck(self, index=None):
-        # Получаем общее количество колод
-        total_decks = await TarotDeck.objects.acount()
+    async def get_deck(self, deck_id=None):
+        # Получаем список всех ID колод
+        deck_ids: List[int] = [deck.id async for deck in TarotDeck.objects.all()]
+        logger.info(f"Получаем колоду с ID {deck_id}")
 
-        if total_decks == 0:
+        if not deck_ids:
             raise ValueError("Нет доступных колод.")
 
-        if index is not None and index >= total_decks:
-            index = None
+        if deck_id is not None and deck_id not in deck_ids:
+            raise ValueError("Указанный ID колоды не существует.")
 
-        if index is None:
-            index = random.randint(0, total_decks - 1)
+        if deck_id is None:
+            deck_id = random.choice(deck_ids)
 
-        # Получаем колоду по индексу
+        # Получаем колоду по ID
         try:
-            qs = TarotDeck.objects.all()[index : index + 1]
-            return await qs.aget()
-
-        except IndexError:
-            # На случай, если колоды были удалены между count() и запросом
-            raise ValueError("Не удалось получить случайную колоду.")
+            return await TarotDeck.objects.aget(id=deck_id)
+        except TarotDeck.DoesNotExist:
+            # На случай, если колода была удалена между получением списка ID и запросом
+            raise ValueError("Не удалось получить колоду.")
 
     async def get_cards(
         self,
@@ -155,9 +158,15 @@ class TarotBot(AbstractBot):
 
     async def send_card(self, update: Update, cards, exclude_cards, deck, major):
         await update.effective_message.reply_media_group(
-            # [InputMediaPhoto(c["img_id"], c["name"]) for c in cards],
             [
-                InputMediaPhoto("https://placecats.com/300/200", c["name"])
+                InputMediaPhoto(
+                    (
+                        "https://placecats.com/300/200"
+                        if settings.TG_DEBUG
+                        else c["img_id"]
+                    ),
+                    c["name"],
+                )
                 for c in cards
             ],
             reply_to_message_id=update.effective_message.message_id,
@@ -171,7 +180,10 @@ class TarotBot(AbstractBot):
                             "Еще карту",
                             callback_data=f'more_{deck.id}_{"#".join(exclude_cards)}_{int(major)}',
                         ),
-                        InlineKeyboardButton("Базовые значения", callback_data=f'desc_{"#".join(exclude_cards)}'),
+                        InlineKeyboardButton(
+                            "Базовые значения",
+                            callback_data=f'desc_{"#".join(exclude_cards)}',
+                        ),
                     ]
                 ]
             ),
@@ -184,140 +196,269 @@ class TarotBot(AbstractBot):
         Обработчик команды /card.
         """
         msg_text = update.message.text
+        logger.info(f"Обработка команды /card с текстом: {msg_text[:100]}")
 
         # Парсинг параметров
-        options = {}
+        options: Dict[str, any] = {}
 
-        # Парсинг количества карт (counter)
-        counter_found = re.search(r"(card)\d", msg_text)
-        options["counter"] = (
-            int(counter_found.group(0).replace("card", "")) if counter_found else 1
-        )
-        options["counter"] = max(1, min(options["counter"], 10))
+        try:
+            # Парсинг количества карт (counter)
+            counter_found = re.search(r"(card)\d", msg_text)
+            options["counter"] = (
+                int(counter_found.group(0).replace("card", "")) if counter_found else 1
+            )
+            options["counter"] = max(1, min(options.get("counter", 1), 10))
+            logger.info(f"Парсинг количества карт: counter={options.get('counter')}")
 
-        # Парсинг колоды (deck)
-        deck_found = re.search(r"deck \d+", msg_text)
-        options["deck"] = (
-            int(deck_found.group(0).replace("deck ", "")) if deck_found else None
-        )
+            # Парсинг колоды (deck)
+            deck_found = re.search(r"deck \d+", msg_text)
+            options["deck"] = (
+                int(deck_found.group(0).replace("deck ", "")) if deck_found else None
+            )
+            logger.info(f"Парсинг колоды: deck={options.get('deck')}")
 
-        # Парсинг переворота карты (flip)
-        options["flip"] = bool(re.search(r"flip", msg_text))
+            # Парсинг переворота карты (flip)
+            options["flip"] = bool(re.search(r"flip", msg_text))
+            logger.info(f"Парсинг переворота карты: flip={options.get('flip')}")
 
-        # Парсинг ID карт (cardIds)
-        card_ids_found = re.findall(r"c(\d+(?:_\d+)*)", msg_text)
-        if card_ids_found:
-            # Преобразуем найденные ID в числа и ограничиваем их от 0 до 77
-            card_ids = [int(c) % 78 for c in card_ids_found[0].split("_")]
-            if len(card_ids) < options["counter"]:
-                temp_id = card_ids[-1]
-                for j in range(len(card_ids), options["counter"]):
-                    temp_id = (temp_id + 1) % 78
-                    card_ids.append(temp_id)
-            options["card_ids"] = card_ids[0 : options["counter"]]
-        else:
-            options["card_ids"] = None
+            # Парсинг ID карт (cardIds)
+            card_ids_found = re.findall(r"c(\d+(?:_\d+)*)", msg_text)
+            if card_ids_found:
+                # Преобразуем найденные ID в числа и ограничиваем их от 0 до 77
+                card_ids = [int(c) % 78 for c in card_ids_found[0].split("_")]
+                if len(card_ids) < options.get("counter", 1):
+                    temp_id = card_ids[-1]
+                    for j in range(len(card_ids), options.get("counter", 1)):
+                        temp_id = (temp_id + 1) % 78
+                        card_ids.append(temp_id)
+                options["card_ids"] = card_ids[: options.get("counter", 1)]
+                logger.info(f"Парсинг ID карт: card_ids={options.get('card_ids')}")
+            else:
+                options["card_ids"] = None
+                logger.info("ID карт не указаны, будут выбраны случайные карты.")
 
-        # Парсинг флага major
-        options["major"] = bool(re.search(r"major", msg_text))
+            # Парсинг флага major
+            options["major"] = bool(re.search(r"major", msg_text))
+            logger.info(f"Парсинг флага major: major={options.get('major')}")
 
-        deck = await self.get_deck(options["deck"])
-        cards = await self.get_cards(
-            deck.id, options["counter"], options["card_ids"], options["major"]
-        )
-        await self.send_card(
-            update, cards, [c["card_id"] for c in cards], deck, options["major"]
-        )
+            # Получение колоды
+            deck = await self.get_deck(options.get("deck"))
+            logger.info(f"Используемая колода: {deck.id if deck else 'не указана'}")
+
+            # Получение карт
+            cards = await self.get_cards(
+                deck.id if deck else None,
+                options.get("counter", 1),
+                options.get("card_ids"),
+                options.get("major", False),
+            )
+            logger.info(f"Получено карт: {len(cards)}")
+
+            # Отправка карт
+            await self.send_card(
+                update,
+                cards,
+                [c["card_id"] for c in cards],
+                deck,
+                options.get("major", False),
+            )
+            logger.info("Карты успешно отправлены.")
+
+        except Exception as e:
+            logger.error(f"Ошибка при обработке команды /card: {e}", exc_info=True)
+            await update.message.reply_text(
+                "Произошла ошибка при обработке вашего запроса. Пожалуйста, попробуйте снова."
+            )
 
     async def handle_more_button(self, update: Update, context: CallbackContext):
         query = update.callback_query
         await query.answer()
+        logger.info(f"Получен callback-запрос: {query.data}")
 
-        _, deck_id, exclude_cards, major = query.data.split("_")
-        exclude_cards = exclude_cards.split("#") if exclude_cards else []
+        try:
+            _, deck_id, exclude_cards, major = query.data.split("_")
+            exclude_cards = exclude_cards.split("#") if exclude_cards else []
+        except ValueError as e:
+            logger.error(f"Ошибка при разборе query.data: {query.data}, ошибка: {e}")
+            await query.edit_message_text("Ошибка обработки запроса.")
+            return
 
         logger.info(
-            f"Запрошена ещё одна карта для колоды {deck_id}. "
-            f"Исключаемые карты: {exclude_cards}"
+            f"Запрошена ещё одна карта для колоды {deck_id}. Исключаемые карты: {exclude_cards}"
         )
 
-        deck = await self.get_deck(int(deck_id))
-        new_card = await self.get_cards(
-            deck_id=int(deck_id),
-            counter=1,
-            card_ids=None,
-            exclude_cards=exclude_cards,
-            major=bool(major),
-        )
+        try:
+            deck = await self.get_deck(int(deck_id))
+            logger.info(f"Загружена колода: {deck}")
+        except Exception as e:
+            logger.error(f"Ошибка загрузки колоды {deck_id}: {e}")
+            await query.edit_message_text("Ошибка загрузки колоды.")
+            return
+
+        try:
+            new_card = await self.get_cards(
+                deck_id=int(deck_id),
+                counter=1,
+                card_ids=None,
+                exclude_cards=exclude_cards,
+                major=bool(major),
+            )
+            logger.info(f"Выбраны новые карты: {new_card}")
+        except Exception as e:
+            logger.error(f"Ошибка получения карт: {e}")
+            await query.edit_message_text("Ошибка получения карт.")
+            return
 
         if not new_card:
+            logger.info("Нет доступных карт для выбора.")
             await query.edit_message_text("Нет доступных карт для выбора.")
             return
+
         full_exclude = exclude_cards + [c["card_id"] for c in new_card]
-        await self.send_card(
-            update,
-            new_card,
-            full_exclude,
-            deck,
-            bool(major),
-        )
+        logger.info(f"Обновленный список исключений: {full_exclude}")
 
+        try:
+            await self.send_card(update, new_card, full_exclude, deck, bool(major))
+            logger.info(f"Карта отправлена: {new_card}")
+        except Exception as e:
+            logger.error(f"Ошибка при отправке карты: {e}")
+            await query.edit_message_text("Ошибка при отправке карты.")
 
-    # Функция для разделения текста на части
     def split_text(self, text, chunk_size=1024):
-        return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+        words = text.split()  # Разбиваем текст на слова
+        chunks = []
+        current_chunk = ""
 
+        for word in words:
+            # Проверяем, не превысит ли добавление слова лимит chunk_size
+            if len(current_chunk) + len(word) + 1 > chunk_size:  # +1 для пробела
+                chunks.append(current_chunk)
+                current_chunk = word  # Начинаем новый кусок с текущего слова
+            else:
+                current_chunk = f"{current_chunk} {word}".strip()  # Добавляем слово
 
-    # Функция для создания инлайн-клавиатуры
-    def create_pagination_keyboard(self, card_id, current_page, total_pages):
-        keyboard = []
-        if current_page > 1:
-            keyboard.append(InlineKeyboardButton("⬅️ Назад", callback_data=f"meaning_{card_id}_{current_page - 1}"))
-        if current_page < total_pages:
-            keyboard.append(InlineKeyboardButton("Вперед ➡️", callback_data=f"meaning_{card_id}_{current_page + 1}"))
-        return InlineKeyboardMarkup([keyboard])
+        if current_chunk:  # Добавляем последний кусок, если он есть
+            chunks.append(current_chunk)
+
+        return chunks
+
+    async def create_pagination_keyboard(
+        self, meaning_type, card_id, current_page, total_pages
+    ):
+        try:
+            logger.info(
+                f"Создание клавиатуры для card_id={card_id}, тип {meaning_type}, страница {current_page} из {total_pages}"
+            )
+
+            # Загружаем значения с prefetch_related
+            all_meanings = ExtendedMeaning.objects.prefetch_related(
+                "tarot_card"
+            ).filter(tarot_card__card_id=card_id)
+
+            # Собираем все значения в список
+            meanings_list = [item.category async for item in all_meanings]
+            meanings_list.append('base')
+            meanings_list.remove(meaning_type)
+            if not meanings_list:
+                logger.warning(f"Для карты {card_id} не найдено значений.")
+                return None  # или вернуть InlineKeyboardMarkup([]), если нужна пустая клавиатура
+
+            logger.info(f"Найдено {len(meanings_list)} значений для карты {card_id}")
+
+            keyboard = []
+
+            # Кнопки навигации
+            paged_row = []
+            if current_page > 1:
+                paged_row.append(
+                    InlineKeyboardButton(
+                        "⬅️ Назад",
+                        callback_data=f"meaning_{meaning_type}_{card_id}_{current_page - 1}",
+                    )
+                )
+            if current_page < total_pages:
+                paged_row.append(
+                    InlineKeyboardButton(
+                        "Вперед ➡️",
+                        callback_data=f"meaning_{meaning_type}_{card_id}_{current_page + 1}",
+                    )
+                )
+            if paged_row:
+                keyboard.append(paged_row)
+
+            # Группируем кнопки по 2 в массив массивов
+            grouped_buttons = [
+                meanings_list[i : i + 2] for i in range(0, len(meanings_list), 2)
+            ]
+
+            # Итерация по сгруппированным данным
+            for group in grouped_buttons:
+                row = [
+                    InlineKeyboardButton(
+                        text=item,
+                        callback_data=f"meaning_{item}_{card_id}_1",
+                    )
+                    for item in group
+                ]
+                keyboard.append(row)
+
+            return InlineKeyboardMarkup(keyboard)
+
+        except Exception as e:
+            logger.error(f"Ошибка в create_pagination_keyboard: {e}", exc_info=True)
+            return None  # Возвращаем None, если произошла критическая ошибка
 
     # Функция для отправки текста с пагинацией
-    async def send_paginated_text(self, update, card_id, text):
+    async def send_paginated_text(self, update: Update, card_id, text):
         # Разделяем текст на части
         text_parts = self.split_text(text)
         total_pages = len(text_parts)
-
+        keyboard = await self.create_pagination_keyboard(
+            "base", card_id, 1, total_pages
+        )
         # Отправляем первую страницу
         await update.effective_message.reply_text(
-            text=text_parts[0],
-            reply_markup=self.create_pagination_keyboard(card_id, 1, total_pages)
+            text=text_parts[0], reply_markup=keyboard
         )
 
     # Обработчик для навигации по страницам
-    async def handle_pagination(self, update:Update, context:CallbackContext):
+    async def handle_pagination(self, update: Update, context: CallbackContext):
         query = update.callback_query
         await query.answer()
 
         # Извлекаем данные из callback_data
-        _, card_id, page = query.data.split('_')
+        _, meaning_type, card_id, page = query.data.split("_")
         page = int(page)
 
-        # Получаем текст карты (здесь предполагается, что текст уже загружен)
-        card = await TarotCard.objects.aget(card_id=card_id)
-        text_parts = self.split_text(card.meaning)
+        if meaning_type == "base":
+            card = await TarotCard.objects.aget(card_id=card_id)
+            text = card.meaning
+        else:
+            cards = ExtendedMeaning.objects.all().prefetch_related("tarot_card")
+            card = await cards.filter(
+                tarot_card__card_id=card_id, category=meaning_type
+            ).aget()
+            text = card.text
+
+        text_parts = self.split_text(text)
+        keyboard = await self.create_pagination_keyboard(
+            meaning_type, card_id, page, len(text_parts)
+        )
 
         # Обновляем сообщение с новой страницей
         await query.edit_message_text(
             text=text_parts[page - 1],
-            reply_markup=self.create_pagination_keyboard(card_id, page, len(text_parts))
+            reply_markup=keyboard,
         )
-
 
     async def handle_desc_button(self, update: Update, context: CallbackContext):
         query = update.callback_query
         await query.answer()
 
         _, cards = query.data.split("_")
-        cards = cards.split('#')
-        
+        cards = cards.split("#")
+
         for c in cards:
             card = await TarotCard.objects.aget(card_id=c)
             await self.send_paginated_text(update, card.card_id, card.meaning)
             time.sleep(0.3)
-

cb3f0e2a7186e3c7b507a665e0bc98f250e18d21
aarizona <ar.ariz.arizona@gmail.com>
2025-09-09 07:08:35 +0300

    logger

--- app/tg_bot/views.py
+++ app/tg_bot/views.py
@@ -31,13 +31,18 @@ def webhook(request, token):
         try:
             # Вы можете просто поместить строку JSON в очередь
             redis_client.rpush(f"bot_messages_queue_{token}", json_str)
-            message = json.loads(json_str)
-            message_content = message.get("message", None)
-            logger.info((
-                f"Сообщение добавлено в очередь: update_id: {message['update_id']}" 
-                f" от {message_content.get('from', {}).get('username')} {message_content.get('from', {}).get('id')}"
-                f" с текстом {(message_content.get('text') or message_content.get('caption'))[0:20]}"
-            ))
+            try:
+                message = json.loads(json_str)
+                message_content = message.get("message", {})
+                logger.info((
+                    f"Сообщение добавлено в очередь: update_id: {message['update_id']}" 
+                    f" от {message_content.get('from', {}).get('username')} {message_content.get('from', {}).get('id')}"
+                    f" с текстом {(message_content.get('text') or message_content.get('caption'))[0:20]}"
+                ))
+            except Exception as e:
+                logger.info((
+                    f"Сообщение добавлено в очередь: update_id: {message['update_id']}" 
+                ))
             return JsonResponse({"status": "ok"})
         except Exception as e:
             logger.error(
